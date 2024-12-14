b4beae1cdea188abfef7ccb99f797b0dd1f612bc
Pervez Ansari <PervezA@emids.com>
2024-12-15 00:03:10 +0530

    Added custom user model, manager, migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Group, Permission
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates a new superuser profile"""
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for user profiles"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Override the default groups and user_permissions fields
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_profiles",  # Custom related_name
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",  # Custom related_name
+        blank=True
+    )
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Returns the full name"""
+        return self.name
+
+    def get_short_name(self):
+        """Returns the short name"""
+        return self.name
+
+    def __str__(self):
+        """Returns a string representation of the user"""
+        return self.email
